8d08ae7eb3dbb67266b5e236c83c02c658cb2de6
Biruk Kebede <biruk.key@gmail.com>
2023-11-12 06:31:05 +0000

    bug fix: country detail

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -6,7 +6,7 @@ const Details = ({ country, setCountry }) => {
 		try {
 			const response = await fetch(url)
 			let data = await response.json()
-			setCountry(data)
+			setCountry(data[0])
 		} catch (err) {
 			console.error(err)
 		}
@@ -17,84 +17,83 @@ const Details = ({ country, setCountry }) => {
 		url = `https://restcountries.com/v3.1/alpha/${code}`
 		loadCountryDataByCode()
 	}
+
+	const languages = Object.values(country.languages)
+
 	return (
 		<div key='details'>
 			<Link to='/' className='back-link'>
 				<i class='fa-solid fa-arrow-left'></i>&nbsp;&nbsp; Back
 			</Link>
-			{country.map((c, i) => {
-				let nativeName = Object.values(Object.values(c.name)[2])
-				let languages = Object.values(c.languages)
-				return (
-					<div key={i} className='country-details-wrapper'>
-						<img
-							className='details-flag'
-							key={i + 1}
-							src={c.flags.png}
-							alt={c.name.common}
-						/>
-						<div className='text-and-buttons'>
-							<div className='main-and-sub-text'>
-								<div className='main-text'>
-									<h3 className='country-name country'>{c.name.common}</h3>
-									<p className='native-name text'>
-										Native name: &nbsp;
-										<span className='light-text'>
-											{nativeName[nativeName.length - 1].common}
-										</span>
-									</p>
-									<p className='population text'>
-										Population: &nbsp;
-										<span className='light-text'>
-											{c.population.toLocaleString()}
-										</span>
-									</p>
-									<p className='region text'>
-										Region: &nbsp;
-										<span className='light-text'>{c.region}</span>
-									</p>
-									<p className='sub-region text'>
-										Sub Region: &nbsp;
-										<span className='light-text'>{c.subregion}</span>
-									</p>
-									<p className='capital text'>
-										Capital: &nbsp;
-										<span className='light-text'>{c.capital}</span>
-									</p>
-								</div>
-								<div className='sub-text'>
-									<p className='tld text'>
-										Top Level Domain: &nbsp;
-										<span className='light-text'>{c.tld}</span>
-									</p>
-									<p className='currencies text'>
-										Currencies: &nbsp;
-										<span className='light-text'>
-											{Object.keys(c.currencies)[0]}
-										</span>
-									</p>
-									<p className='languages text'>
-										Languages: &nbsp;
-										<span className='light-text'>{languages.join(', ')}</span>
-									</p>
-								</div>
-							</div>
-							<div className='border-countries'>
-								<p className='text'>Border Countries: &nbsp;</p>
-								<div className='borders'>
-									{c.borders
-										? c.borders.map(b => (
-												<button className='border' onClick={loadBorderCountry}>
-													{b}
-												</button>
-										  ))
-										: ''}
-								</div>
-							</div>
+			<div key={country.name} className='country-details-wrapper'>
+				<img
+					className='details-flag'
+					src={country.flags.png}
+					alt={country.name.common}
+				/>
+				<div className='text-and-buttons'>
+					<div className='main-and-sub-text'>
+						<div className='main-text'>
+							<h3 className='country-name country'>{country.name.common}</h3>
+							<p className='native-name text'>
+								Native name: &nbsp;
+								<span className='light-text'>
+									{Object.values(country.name.nativeName)[0].official}
+								</span>
+							</p>
+							<p className='population text'>
+								Population: &nbsp;
+								<span className='light-text'>
+									{country.population.toLocaleString()}
+								</span>
+							</p>
+							<p className='region text'>
+								Region: &nbsp;
+								<span className='light-text'>{country.region}</span>
+							</p>
+							<p className='sub-region text'>
+								Sub Region: &nbsp;
+								<span className='light-text'>{country.subregion}</span>
+							</p>
+							<p className='capital text'>
+								Capital: &nbsp;
+								<span className='light-text'>{country.capital}</span>
+							</p>
+						</div>
+						<div className='sub-text'>
+							<p className='tld text'>
+								Top Level Domain: &nbsp;
+								<span className='light-text'>{country.tld}</span>
+							</p>
+							<p className='currencies text'>
+								Currencies: &nbsp;
+								{country.currencies && (
+									<span className='light-text'>
+										{Object.keys(country.currencies)[0]}
+									</span>
+								)}
+							</p>
+							<p className='languages text'>
+								Languages: &nbsp;
+								<span className='light-text'>{languages.join(', ')}</span>
+							</p>
 						</div>
 					</div>
-				)
-			})}
+					<div className='border-countries'>
+						<p className='text'>Border Countries: &nbsp;</p>
+						<div className='borders'>
+							{country.borders
+								? country.borders.map(b => (
+										<button className='border' onClick={loadBorderCountry}>
+											{b}
+										</button>
+								  ))
+								: ''}
+						</div>
+					</div>
+				</div>
+			</div>
+			)
 		</div>
 	)
 }
